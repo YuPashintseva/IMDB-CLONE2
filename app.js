5329b4d2cb6df3f13ed4efac2de19e84fc4e6855
YuPashintseva <juliyapashintseva@gmail.com>
2021-01-26 22:34:30 +0600

    fix: production mode

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json({extended: true}));
 app.use('/api/auth', require('./routes/auth.routes'));
 //app.use('/api/link', require('./routes/link.routes'));
 
-if (process.env.NODE_ENV === 'prodaction') {
+if (process.env.NODE_ENV === 'production') {
     app.use('/',express.static(path.join(__dirname, 'client', 'build')))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
